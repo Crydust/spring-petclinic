cd2290edc08d7bb38c3e847300632013f02ec69f
Kristof Neirynck <k.neirynck@gmail.com>
2020-05-24 18:37:16 +0200

    Add integration test

---
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerIntegrationTests.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.petclinic.owner;
+
+import com.vladmihalcea.sql.SQLStatementCountValidator;
+import net.ttddyy.dsproxy.listener.DataSourceQueryCountListener;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+import static com.vladmihalcea.sql.SQLStatementCountValidator.assertSelectCount;
+
+@SpringBootTest
+class OwnerControllerIntegrationTests {
+
+	@TestConfiguration
+	public static class DataSourceConfiguration {
+
+		@Bean
+		public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+			return ProxyDataSourceBuilder.create(dataSourceProperties.initializeDataSourceBuilder().build())
+					.listener(new DataSourceQueryCountListener()).build();
+		}
+
+	}
+
+	@Autowired
+	OwnerController owners;
+
+	@Test
+	void shouldRetrieveAllPetsInOneQuery() {
+		SQLStatementCountValidator.reset();
+		owners.showOwner(3);
+		assertSelectCount(3);
+	}
+
+}
